dbacb65275b1b718d00fa2dc3f312751f7ff1a60
bruno lamarlere <blamarlere@ordesoft.com>
2022-09-19 07:58:01 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import pyworld3
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    pyworld3.hello_world3()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
